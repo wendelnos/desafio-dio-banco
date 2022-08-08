a7cc27784138f0e2ced8859dccf05e2cf8716662
wendelnos <wendelnos@gmail.com>
2022-08-08 19:36:32 -0300

    desafio de projeto de JAVA

---
+++ README.md
@@ -0,0 +1,9 @@
+**Projeto Banco digital**
+
+Realizei algumas modificações no projeto original. Pelo tempo, optei em não realizar alguma nova classe, e a instalação do Lombok firará para um segundo momento. 
+
+
+
+
+
+
---
+++ src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		Cliente wendel = new Cliente();
+		wendel.setNome("Wendel");
+		Conta cc = new ContaCorrente(wendel);
+		
+		
+		Conta poupanca = new ContaPoupanca(wendel);
+		
+		cc.depositar(100);
+		cc.transferir(100, poupanca);
+		cc.imprimirExtrato();
+		poupanca.imprimirExtrato();
+	}
+}
+
